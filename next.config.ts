f01435ab895de6da6ec90464291b710da6a18767
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-04 13:51:12 +0100

    prod deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath =
-  // process.env.DEPLOY_ENV === "PROD" ? "/nextjstest" : "";
-
-  process.env.NODE_ENV === "production" ? "/nextjstest" : "";
+const basePath = process.env.DEPLOY_ENV === "PROD" ? "/nextjstest" : "";
 
 const nextConfig: NextConfig = {
   images: {
