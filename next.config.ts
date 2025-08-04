366e91780eb6c01738c8242bb7bbe6296a0de56a
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-04 13:12:05 +0100

    updating DEPLOY_ENV

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "/nextjsApp" : "";
+const basePath = process.env.DEPLOY_ENV === "PROD" ? "/nextjsApp" : "";
 
 const nextConfig: NextConfig = {
   images: {
