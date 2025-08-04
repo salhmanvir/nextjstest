1dc57138dee292bec1328b8e427a2011b306dfc7
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-04 15:38:08 +0100

    adding / to url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "nextjstest" : "";
+const basePath = process.env.NODE_ENV === "production" ? "/nextjstest" : "";
 
 const nextConfig: NextConfig = {
   images: {
