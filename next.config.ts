9a4678f0563362113d102d4af850eb042ab899f6
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-04 13:20:44 +0100

    updating url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.DEPLOY_ENV === "PROD" ? "/nextjsApp" : "";
+const basePath = process.env.DEPLOY_ENV === "PROD" ? "/nextjstest" : "";
 
 const nextConfig: NextConfig = {
   images: {
